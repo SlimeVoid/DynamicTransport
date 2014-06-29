1e76d3bb77a3843a77f454ef24728ac83476886b
Tarig0 <alcook16@gmail.com>
2014-06-29 15:33:42 -0400

    Bump Version and changes to build script note to self remember to clean before build

--- src/main/java/net/slimevoid/dynamictransport/core/lib/CoreLib.java
+++ src/main/java/net/slimevoid/dynamictransport/core/lib/CoreLib.java
@@ -9,8 +9,8 @@ public class CoreLib {
     public static final String MOD_RESOURCES       = "dynamictransport";
     public static final String MOD_NAME            = "Dynamic Transport Mod";
     public static final String MOD_VERSION         = "@VERSION@";
-    public static final String MOD_DEPENDENCIES    = "";
-    public static final String ACTUAL_DEPENDENCIES = "required-after:Forge@[@FORGEVERSION@,),SlimevoidLib@[@SLIMEVERSION@,)";
+    public static final String MOD_DEPENDENCIES    = "required-after:SlimevoidLib";
+    public static final String ACTUAL_DEPENDENCIES = "";
     public static final String MOD_CHANNEL         = "DYNAMICTRANSPORT";
     public static final String CLIENT_PROXY        = "net.slimevoid.dynamictransport.client.proxy.ClientProxy";
     public static final String COMMON_PROXY        = "net.slimevoid.dynamictransport.proxy.CommonProxy";
